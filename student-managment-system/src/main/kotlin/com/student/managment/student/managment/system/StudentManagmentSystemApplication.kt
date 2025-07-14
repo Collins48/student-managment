43e2b5a78e0a1601d8e3e5b8fa0269b33a40bacd
Collins48 <collinkimutai648@gmail.com>
2025-07-14 10:32:31 +0300

    Initial commit

---
+++ student-managment-system/src/main/kotlin/com/student/managment/student/managment/system/StudentManagmentSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.student.managment.student.managment.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentManagmentSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentManagmentSystemApplication>(*args)
+}
---
+++ student-managment-system/src/test/kotlin/com/student/managment/student/managment/system/StudentManagmentSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.student.managment.student.managment.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudentManagmentSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
